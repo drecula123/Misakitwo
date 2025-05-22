19f6217921546fe25cb894faa4ca955dcb169985
DeepVikky88 <radheymusic51@gmail.com>
2025-05-22 09:28:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,9 +16,8 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-# Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://tubed.okflix.top') ## E.G https://yt.okflix.
-YT_API_KEY = "freekey15052025"
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", "NxGBNexGenBots31bc9e") # youtube song api key, get it from https://t.me/its_deep_ydv
 
 # Other vaes
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
