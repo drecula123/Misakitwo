3167012738b6c483c155c7047244908805d34d14
DeepVikky88 <radheymusic51@gmail.com>
2025-05-26 16:14:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,10 +16,6 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", "30DxNexGenBots0d1419") # youtube song api key, get it from https://t.me/its_deep_ydv
-
-
 # Other vaes
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
 
